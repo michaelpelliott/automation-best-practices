d5c69b909d7e87d2a4ccc91f980798b70d38053f
Michael Elliott <melliott@tandemdiabetes.com>
2021-10-14 10:34:34 -0600

    added 3 tests, jerome wouldn't like this commit style

--- testing-for-charity/my-react-app/cypress/integration/exercise.spec.js
+++ testing-for-charity/my-react-app/cypress/integration/exercise.spec.js
@@ -5,16 +5,19 @@ describe('Exercise', () => {
         /** Your code below */
     
         //1. Use cy.visit('/') to go to the app url
+        cy.visit('/');
         //2. Use cy.get('.App-link').should('be.visible') to assert valid state
+        cy.get('.App-link').should('be.visible');
         /** Your code above */
     })
     
     it('link goes to ultimateqa', ()=> {
         /** Your code below */
-    
+
         // 1. Use cy.visit('/') to go to the app url
+        cy.visit('/');
         // 2. cy.get('').should('have.attr', 'href').and('include', 'ultimateqa.com')
-    
+        cy.get('.App-link').should('have.attr', 'href').and('include','ultimateqa.com');
         /** Your code above */
     })
     
@@ -22,8 +25,9 @@ describe('Exercise', () => {
         /** Your code below */
     
         // 1. Cmon, you know how to open the url now :)
+        cy.visit('/');
         // 2. cy.get('').should('have.attr', 'WHAT').and('include', 'WHAT VALUE')
-    
+        cy.get('.App-link').should('have.attr', 'target').and('include', '_blank');
         /** Your code above */
     })  
 })
